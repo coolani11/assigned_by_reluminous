9f3dd6eb0ac5bc98bc440079bae15c0f5dda5640
Aniket Vishwakarma <107803823+coolani11@users.noreply.github.com>
2023-03-25 23:59:36 +0530

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,8 +10,7 @@ export default {
   
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - nuxt-vuetify-firebase-auth',
-    title: 'nuxt-vuetify-firebase-auth',
+    title: 'Todo-App-Reluminous_Tech',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
